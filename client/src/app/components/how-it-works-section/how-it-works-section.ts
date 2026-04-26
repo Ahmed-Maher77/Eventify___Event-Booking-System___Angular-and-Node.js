527adec54e3c4a5abc570983f8d16e38f7479b96
“Ahmed <ahmedmaheraljwhry057@gmail.com>
2026-04-26 15:52:20 +0300

    Add how-it-works-section component with HTML, SCSS, and TypeScript files for displaying the section content.

---
+++ client/src/app/components/how-it-works-section/how-it-works-section.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-how-it-works-section',
+  imports: [],
+  templateUrl: './how-it-works-section.html',
+  styleUrl: './how-it-works-section.scss',
+})
+export class HowItWorksSection {}
