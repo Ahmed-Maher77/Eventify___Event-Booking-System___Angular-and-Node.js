556477dfab4d79f83c998bc950809046eb5e2bc9
“Ahmed <ahmedmaheraljwhry057@gmail.com>
2026-04-26 15:55:37 +0300

    Add BookingDetailsPage and BookingsSuccessPage components with HTML and TypeScript files for displaying booking details and success messages.

---
+++ client/src/app/pages/bookings-success/bookings-success.page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bookings-success-page',
+  standalone: true,
+  templateUrl: './bookings-success.page.html'
+})
+export class BookingsSuccessPage {}
