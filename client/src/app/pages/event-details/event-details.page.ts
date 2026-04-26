825c5f08f0e010ee9c9fcf26739eff5256b5d233
“Ahmed <ahmedmaheraljwhry057@gmail.com>
2026-04-26 16:42:58 +0300

    Add EventDetailsPage component with HTML and TypeScript files for displaying event details. Introduce EventsPage component with HTML for listing events.

---
+++ client/src/app/pages/event-details/event-details.page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-details-page',
+  standalone: true,
+  templateUrl: './event-details.page.html'
+})
+export class EventDetailsPage {}
