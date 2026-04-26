de359b0140bcdac7a4e456ae526eed17e601789d
“Ahmed <ahmedmaheraljwhry057@gmail.com>
2026-04-26 15:32:33 +0300

    Add explore-categories-section component with HTML, SCSS, and TypeScript files

---
+++ client/src/app/components/explore-categories-section/explore-categories-section.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-explore-categories-section',
+  imports: [],
+  templateUrl: './explore-categories-section.html',
+  styleUrl: './explore-categories-section.scss',
+})
+export class ExploreCategoriesSection {}
