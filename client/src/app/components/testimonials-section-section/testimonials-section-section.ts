081d42b8e878c4a11e51a86fc4af2acc731dbe98
“Ahmed <ahmedmaheraljwhry057@gmail.com>
2026-04-26 15:52:50 +0300

    Add testimonials-section-section component with HTML, SCSS, and TypeScript files for displaying testimonials.

---
+++ client/src/app/components/testimonials-section-section/testimonials-section-section.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-testimonials-section-section',
+  imports: [],
+  templateUrl: './testimonials-section-section.html',
+  styleUrl: './testimonials-section-section.scss',
+})
+export class TestimonialsSectionSection {}
