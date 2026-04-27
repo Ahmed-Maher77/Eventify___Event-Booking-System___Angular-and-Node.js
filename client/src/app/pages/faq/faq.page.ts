d1d733ca441c42e1169b23f45903e8ed49f24a06
“Ahmed <ahmedmaheraljwhry057@gmail.com>
2026-04-27 10:24:18 +0300

    Revamp FAQ Page layout and styles for improved user experience. Update HTML structure to include a subtitle and enhance accessibility, and add SCSS styling for a cohesive design.

--- client/src/app/pages/faq/faq.page.ts
+++ client/src/app/pages/faq/faq.page.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-faq-page',
   standalone: true,
-  templateUrl: './faq.page.html'
+  templateUrl: './faq.page.html',
+  styleUrl: '../shared/static-info-page.scss'
 })
 export class FaqPage {}
