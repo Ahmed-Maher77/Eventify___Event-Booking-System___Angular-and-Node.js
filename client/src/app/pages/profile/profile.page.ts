e5fef629a555c074533254bddc0c21a50b842672
“Ahmed <ahmedmaheraljwhry057@gmail.com>
2026-04-27 14:53:20 +0300

    Revamp profile page layout and styling by introducing a new title and subtitle for better user guidance. Add SCSS reference for improved styling management.

--- client/src/app/pages/profile/profile.page.ts
+++ client/src/app/pages/profile/profile.page.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-profile-page',
   standalone: true,
-  templateUrl: './profile.page.html'
+  templateUrl: './profile.page.html',
+  styleUrl: '../../../sass/components/static-info-page.scss'
 })
 export class ProfilePage {}
