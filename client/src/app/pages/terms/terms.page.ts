76aafd22697e5f3779ab04c538a457da59b1f2b4
“Ahmed <ahmedmaheraljwhry057@gmail.com>
2026-04-27 10:24:05 +0300

    Revamp Terms Page layout and styles for improved user experience. Update HTML structure to include a subtitle and enhance accessibility, and add SCSS styling for a cohesive design.

--- client/src/app/pages/terms/terms.page.ts
+++ client/src/app/pages/terms/terms.page.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-terms-page',
   standalone: true,
-  templateUrl: './terms.page.html'
+  templateUrl: './terms.page.html',
+  styleUrl: '../shared/static-info-page.scss'
 })
 export class TermsPage {}
