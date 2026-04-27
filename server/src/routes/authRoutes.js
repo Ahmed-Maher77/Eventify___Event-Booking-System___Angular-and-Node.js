9daab826f1d2aab4577501fccaa9a0ead44ea141
“Ahmed <ahmedmaheraljwhry057@gmail.com>
2026-04-27 07:44:54 +0300

    Implement user profile picture upload in registration, allowing image file uploads or URL input. Update User model to include pictureUrl and picturePublicId fields, and adjust authController and routes accordingly.

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,10 +1,11 @@
 import  express  from 'express';
 import {register,login} from "../controllers/authController.js"
+import { uploadImage } from "../config/multerConfig.js";
 
 
 const router=express.Router();
 
-router.post("/register",register)
+router.post("/register", uploadImage.single("image"), register)
 router.post("/login",login)
 
 
