977cc655da711600665615b450bc2e15baf99685
“Ahmed <ahmedmaheraljwhry057@gmail.com>
2026-04-27 15:56:30 +0300

    Revamp Events Page layout with new HTML structure and styling. Introduce a section for static information, enhancing the presentation of upcoming events and user engagement.

--- client/src/app/pages/events/events.page.ts
+++ client/src/app/pages/events/events.page.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-events-page',
   standalone: true,
-  templateUrl: './events.page.html'
+  templateUrl: './events.page.html',
+  styleUrl: '../../../sass/components/static-info-page.scss'
 })
 export class EventsPage {}
