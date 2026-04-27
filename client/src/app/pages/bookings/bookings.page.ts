8ef6651d40a12ed18d4cfd605d2ce61a4ceb4185
“Ahmed <ahmedmaheraljwhry057@gmail.com>
2026-04-27 14:52:40 +0300

    Update bookings page layout and styling to enhance user experience with a new title and subtitle. Add SCSS reference for improved styling management.

--- client/src/app/pages/bookings/bookings.page.ts
+++ client/src/app/pages/bookings/bookings.page.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-bookings-page',
   standalone: true,
-  templateUrl: './bookings.page.html'
+  templateUrl: './bookings.page.html',
+  styleUrl: '../../../sass/components/static-info-page.scss'
 })
 export class BookingsPage {}
