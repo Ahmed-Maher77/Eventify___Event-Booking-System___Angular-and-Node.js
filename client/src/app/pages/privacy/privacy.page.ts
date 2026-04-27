b73d37fad16b05366789e12c0945971948e51769
“Ahmed <ahmedmaheraljwhry057@gmail.com>
2026-04-27 10:24:28 +0300

    Revamp Privacy Page layout and styles for improved user experience. Update HTML structure to include a subtitle and enhance accessibility, and add SCSS styling for a cohesive design.

--- client/src/app/pages/privacy/privacy.page.ts
+++ client/src/app/pages/privacy/privacy.page.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-privacy-page',
   standalone: true,
-  templateUrl: './privacy.page.html'
+  templateUrl: './privacy.page.html',
+  styleUrl: '../shared/static-info-page.scss'
 })
 export class PrivacyPage {}
