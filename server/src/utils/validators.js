a48d3a0b5a4f4462cd93d6a0a6a252f6c9922567
MohamedAwad33 <95631364+MohamedAwad33@users.noreply.github.com>
2026-05-07 18:14:31 +0300

    create a validator for creating a new admin

--- server/src/utils/validators.js
+++ server/src/utils/validators.js
@@ -318,6 +318,38 @@ const validateQuery = [
 ];
 
 /**
+ * Validate admin creation
+ */
+const validateAdminCreation = [
+  body('name')
+    .trim()
+    .notEmpty().withMessage('Name is required')
+    .isLength({ min: 2, max: 50 }).withMessage('Name must be between 2 and 50 characters'),
+
+  body('email')
+    .trim()
+    .notEmpty().withMessage('Email is required')
+    .isEmail().withMessage('Please provide a valid email address')
+    .normalizeEmail(),
+
+  body('password')
+    .notEmpty().withMessage('Password is required')
+    .isLength({ min: 6 }).withMessage('Password must be at least 6 characters')
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])/).withMessage('Password must contain uppercase, lowercase, number, and special character'),
+
+  body('confirmPassword')
+    .notEmpty().withMessage('Confirm password is required')
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Passwords do not match');
+      }
+      return true;
+    }),
+
+  handleValidationErrors
+];
+
+/**
  * Validate admin users list query (search/filter/sort)
  */
 const validateAdminUsersQuery = [
@@ -383,6 +415,7 @@ export {
   validateContactMessageStatusUpdate,
   validateNewsletterSubscriberStatusUpdate,
   validateAdminUsersQuery,
+  validateAdminCreation,
   validateObjectId,
   validateQuery,
   handleValidationErrors,
